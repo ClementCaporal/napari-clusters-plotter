82f74dfa96272a51c6645c908a4b49feb704006d
thorstenwagner <twa1@posteo.de>
2023-03-14 09:10:57 +0100

    use unique elements of cluster IDs to create colormap

--- napari_clusters_plotter/_plotter.py
+++ napari_clusters_plotter/_plotter.py
@@ -433,7 +433,7 @@ class PlotterWidget(QMainWindow):
                     if prediction >= 0
                     else [0, 0, 0, 0]
                 )
-                for prediction in self.cluster_ids
+                for prediction in np.unique(self.cluster_ids)
             }
             # take care of background label
             cmap_dict[0] = [0, 0, 0, 0]
